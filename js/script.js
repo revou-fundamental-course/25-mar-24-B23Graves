2654b602d5a30df74622b34afa2004dbe2ef8f82
B23Graves <165409398+B23Graves@users.noreply.github.com>
2024-04-03 21:48:24 -0700

    First Commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var varible = "A"
+let letible = "B"
+const constanta = "C"
+
+// Contoh yang standar cocok untuk yang belajar
+if (varible == 'A') {
+    console.log(varible);
+}
+
+// Tanda kurung
+if ((letible == 'B')) console.log(letible);
+
+// operator if ada 2 style
+constanta == 'C' 
+? console.log(constanta) 
+: console.log('Bukan C')
