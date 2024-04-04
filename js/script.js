99754ece8fa9102dccc42dc0b0f4a066068c8a1b
B23Graves <165409398+B23Graves@users.noreply.github.com>
2024-04-04 22:19:40 +0700

    First commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
-var varible = "A"
-let letible = "B"
-const constanta = "C"
-
-// Contoh yang standar cocok untuk yang belajar
-if (varible == 'A') {
-    console.log(varible);
+function calculate(){
+    let weightInput = document.getElementById('weight-input').value; 
+    console.log(weightInput);
+    
+    if(weightInput == '') {
+        alert('Harap isi berat badan anda')
+        console.log('kosong');
+    } else {
+        console.log('ada isi');
+    }
 }
-
-// Tanda kurung
-if ((letible == 'B')) console.log(letible);
-
-// operator if ada 2 style
-constanta == 'C' 
-? console.log(constanta) 
-: console.log('Bukan C')
